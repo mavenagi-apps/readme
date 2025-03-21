c2ce8c99da9bfdc01c2c88a4de25baff7ee96f5b
Sylvain Galineau <sylvain@mavenagi.com>
2025-03-21 07:06:53 -0700

    feat: update Inngest app name to prod

--- src/inngest/client.ts
+++ src/inngest/client.ts
@@ -2,9 +2,9 @@ import { EventSchemas, Inngest } from "inngest";
 import { z } from "zod";
 
 export const inngest = new Inngest({
-    id: "app/readme-develop",
+    id: "app/readme",
     schemas: new EventSchemas().fromZod({
-        "app/readme-develop/process": {
+        "app/readme/process": {
             data: z.object({
                 organizationId: z.string(),
                 agentId: z.string(),
