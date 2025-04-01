98cce26f8300c20c05bee66278683fdf64802510
Sylvain Galineau <sylvain@mavenagi.com>
2025-04-01 10:18:54 -0700

    chore: switch develop branch back to develop Inngest IDs

--- src/inngest/constants.ts
+++ src/inngest/constants.ts
@@ -1,4 +1,4 @@
-export const INNGEST_EVENT = 'app/readme/process';
+export const INNGEST_EVENT = 'app/readme-develop/process';
 
 export const README_API_BASE_URL = 'https://dash.readme.com/api/v1';
 
