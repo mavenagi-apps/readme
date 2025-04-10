7b93046588375631d81e385a2af5114dda789652
Sylvain Galineau <sylvain@mavenagi.com>
2025-04-10 15:55:34 -0400

    feat: use main branch Inngest names

--- src/inngest/constants.ts
+++ src/inngest/constants.ts
@@ -1,5 +1,5 @@
-export const INNGEST_EVENT = 'app/readme-develop/process';
+export const INNGEST_EVENT = 'app/readme/process';
 
 export const README_API_BASE_URL = 'https://dash.readme.com/api/v1';
 
-export const KNOWLEDGE_BASE_ID = 'readme-develop';
+export const KNOWLEDGE_BASE_ID = 'readme';
